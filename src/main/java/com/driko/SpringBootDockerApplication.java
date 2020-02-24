efcb9fae981e97cd41ffa5e7466baa84c6849708
Adriano da Silva Rocha <adriano_rocha15@hotmail.com>
2020-02-23 23:03:25 -0300

    Projeto de exemplo do Spring Boot com Docker

---
+++ src/main/java/com/driko/SpringBootDockerApplication.java
@@ -0,0 +1,23 @@
+package com.driko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(value="/hello")
+public class SpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+	
+	@GetMapping
+	public String hello() {
+		return "Hello, Spring Boot with container!";
+	} 
+
+}
---
+++ src/test/java/com/driko/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
